874872b05cd98ca4b23906577e5f12305e5d35c6
Francisco Zorrilla <fazorrilla@pop-os.localdomain>
2021-05-03 03:57:48 -0400

    Todos los errores 422

--- src/routes/albums/addTrack.js
+++ src/routes/albums/addTrack.js
@@ -5,38 +5,48 @@ export const addTrack = async (ctx) => {
   console.log(ctx.params);
   const args = ctx.request.body;
   console.log(args);
-  const idtrac = btoa(`${args.name}:${ctx.params.albumId}`);
-  const idtrack = idtrac.substring(0, 22);
-  console.log(idtrack);
   const album = await prisma.album.findUnique({
     where: {
       id: ctx.params.albumId,
     },
   });
-  const ifExist = await prisma.track.findUnique({
-    where: {
-      id: idtrack,
-    },
-  });
-  if (ifExist) {
-    ctx.body = ifExist;
-    ctx.status = 409;
-  } else {
-    const track = await prisma.track.create({
-      data: {
-        name: args.name,
-        duration: args.duration,
-        timesPlayed: args.timesPlayed,
-        artist: {
-          connect: { id: album.artistId },
-        },
-        album: {
-          connect: { id: ctx.params.albumId },
-        },
+  if (album) {
+    const idtrac = btoa(`${args.name}:${ctx.params.albumId}`);
+    const idtrack = idtrac.substring(0, 22);
+    console.log(idtrack);
+    const album = await prisma.album.findUnique({
+      where: {
+        id: ctx.params.albumId,
+      },
+    });
+    const ifExist = await prisma.track.findUnique({
+      where: {
         id: idtrack,
       },
     });
-    ctx.body = track;
-    ctx.status = 201;
+    if (ifExist) {
+      ctx.body = ifExist;
+      ctx.status = 409;
+    } else {
+      const track = await prisma.track.create({
+        data: {
+          name: args.name,
+          duration: args.duration,
+          timesPlayed: args.timesPlayed,
+          artist: {
+            connect: { id: album.artistId },
+          },
+          album: {
+            connect: { id: ctx.params.albumId },
+          },
+          id: idtrack,
+        },
+      });
+      ctx.body = track;
+      ctx.status = 201;
+    }
+  } else {
+    ctx.body = 'No existe el album';
+    ctx.status = 422;
   }
 };
--- src/routes/artists/addAlbum.js
+++ src/routes/artists/addAlbum.js
@@ -5,29 +5,39 @@ export const addAlbum = async (ctx) => {
   console.log(ctx.params);
   const args = ctx.request.body;
   console.log(args);
-  const idalbu = btoa(`${args.name}:${ctx.params.artistId}`);
-  const idalbum = idalbu.substring(0, 22);
-  console.log(idalbum);
-  const ifExist = await prisma.album.findUnique({
+  const artist = await prisma.artist.findUnique({
     where: {
-      id: idalbum,
+      id: ctx.params.artistId,
     },
   });
-  if (ifExist) {
-    ctx.body = ifExist;
-    ctx.status = 409;
-  } else {
-    const album = await prisma.album.create({
-      data: {
-        name: args.name,
-        genre: args.genre,
-        artist: {
-          connect: { id: ctx.params.artistId },
-        },
+  if (artist) {
+    const idalbu = btoa(`${args.name}:${ctx.params.artistId}`);
+    const idalbum = idalbu.substring(0, 22);
+    console.log(idalbum);
+    const ifExist = await prisma.album.findUnique({
+      where: {
         id: idalbum,
       },
     });
-    ctx.body = album;
-    ctx.status = 201;
+    if (ifExist) {
+      ctx.body = ifExist;
+      ctx.status = 409;
+    } else {
+      const album = await prisma.album.create({
+        data: {
+          name: args.name,
+          genre: args.genre,
+          artist: {
+            connect: { id: ctx.params.artistId },
+          },
+          id: idalbum,
+        },
+      });
+      ctx.body = album;
+      ctx.status = 201;
+    }
+  } else {
+    ctx.body = 'No existe el artista';
+    ctx.status = 422;
   }
 };
